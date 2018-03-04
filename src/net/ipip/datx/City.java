5e8202e4077d823042154e2d5c169a61b2bec498
ipipdotnet <frk@ipip.net>
2018-03-04 17:38:36 +0800

    update code

---
+++ src/net/ipip/datx/City.java
@@ -0,0 +1,125 @@
+package net.ipip.datx;
+
+import java.nio.charset.Charset;
+import java.io.FileInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+
+public class City
+{
+    private File file;
+
+    private byte[] data;
+
+    private long indexSize;
+
+    public City(String name) throws IOException
+    {
+        Path path = Paths.get(name);
+        data = Files.readAllBytes(path);
+
+        indexSize = bytesToLong(data[0], data[1], data[2], data[3]);   
+    }
+
+    public String[] find(String ips) {
+        
+        long val = ip2long(ips);
+        int start = 262148;
+        int low = 0;
+        int mid = 0;
+        int high = new Long((indexSize - 262144 - 262148) / 9).intValue();
+        int pos = 0;
+        while (low <= high)
+        {
+            mid = Integer.valueOf((low + high) / 2);
+            pos = mid * 9;
+
+            long s = 0;
+            if (mid > 0)
+            {
+                int pos1 = (mid - 1) * 9;
+                s = bytesToLong(data[start + pos1], data[start + pos1+1], data[start + pos1+2], data[start + pos1+3]);
+            }
+
+            long end = bytesToLong(data[start + pos], data[start + pos+1], data[start + pos+2], data[start + pos+3]);
+            if (val > end) {
+                low = mid + 1;
+            } else if (val < s) {
+                high = mid - 1;
+            } else {
+
+                byte b =0;
+                long off = bytesToLong(b, data[start+pos+6],data[start+pos+5],data[start+pos+4]);
+                long len = bytesToLong(b, b, data[start+pos+7], data[start+pos+8]);
+
+                int offset = new Long(off - 262144 + indexSize).intValue();
+
+                byte[] loc = Arrays.copyOfRange(data, offset, offset+new Long(len).intValue());
+
+                return new String(loc, Charset.forName("UTF-8")).split("\t", -1);
+            }
+        }
+
+        return null;
+    }
+
+    public static void main(String []args)
+    {
+        try {
+            City c = new City("c:/work/tiantexin/17mon/mydata4vipday4.datx");
+
+            System.out.println(Arrays.toString(c.find("8.8.8.8")));
+            System.out.println(Arrays.toString(c.find("255.255.255.255")));
+        } catch (IOException ioex){
+
+        }
+    }
+
+    
+    private static long bytesToLong(byte a, byte b, byte c, byte d) {
+        return int2long((((a & 0xff) << 24) | ((b & 0xff) << 16) | ((c & 0xff) << 8) | (d & 0xff)));
+    }
+
+    private static int str2Ip(String ip)  {
+        String[] ss = ip.split("\\.");
+        int a, b, c, d;
+        a = Integer.parseInt(ss[0]);
+        b = Integer.parseInt(ss[1]);
+        c = Integer.parseInt(ss[2]);
+        d = Integer.parseInt(ss[3]);
+        return (a << 24) | (b << 16) | (c << 8) | d;
+    }
+
+    private static int str2Ip2(String ip)  {
+        try {
+            byte[] bytes = java.net.InetAddress.getByName(ip).getAddress();
+
+            return ((bytes[0] & 0xFF) << 24) |
+                    ((bytes[1] & 0xFF) << 16) |
+                    ((bytes[2] & 0xFF) << 8) |
+                    bytes[3];
+        } catch (java.net.UnknownHostException e) {
+
+        }
+
+        return 0;
+    }
+
+
+    private static long ip2long(String ip)  {
+        return int2long(str2Ip(ip));
+    }
+
+    private static long int2long(int i) {
+        long l = i & 0x7fffffffL;
+        if (i < 0) {
+            l |= 0x080000000L;
+        }
+        return l;
+    }
+ }
